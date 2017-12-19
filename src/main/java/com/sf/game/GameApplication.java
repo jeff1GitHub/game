fa0ab68365f0941457d21134eadfe841b6e21341
jeff <jeff@orientalgame99.com>
2017-12-20 00:28:49 +0800

    init

---
+++ src/main/java/com/sf/game/GameApplication.java
@@ -0,0 +1,19 @@
+package com.sf.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class GameApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GameApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameApplication.class, args);
+	}
+}
